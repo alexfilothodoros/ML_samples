64896921f3031e335cd191e44aa0b528c71d5e1e
Alexandros Filothodoros <alexfilothodoros@gmail.com>
2024-03-13 13:03:11 +0100

    Create rag+_llamaindex_latest.py

---
+++ LLM/RAG/rag+_llamaindex_latest.py
@@ -0,0 +1,38 @@
+import os
+import openai
+from dotenv import load_dotenv
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+load_dotenv()
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+print(OPENAI_API_KEY)
+
+documents = SimpleDirectoryReader("data").load_data()
+index = VectorStoreIndex.from_documents(documents)
+query_engine = index.as_query_engine()
+query = "Does the license allow me to use the software commercially?"
+response = query_engine.query(query)
+print(response)
+
+
+index.storage_context.persist()
+
+
+PERSIST_DIR = "./storage"
+if not os.path.exists(PERSIST_DIR):
+    documents = SimpleDirectoryReader("data").load_data()
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist(persist_dir=PERSIST_DIR)
+else:
+    storage_context = StorageContext.from_defaults(persist_dir=PERSIST_DIR)
+    index = load_index_from_storage(storage_context)
+
+query_engine = index.as_query_engine()
+response = query_engine.query(query)
+print(response)
